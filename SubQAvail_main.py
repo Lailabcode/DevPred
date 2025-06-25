bedb108ce00e0922060ed0dfb7e629f965006a35
IEn Wu <iwu@stevens.edu>
2025-06-25 15:13:41 -0400

    model tersttt

--- SubQAvail_main.py
+++ SubQAvail_main.py
@@ -70,7 +70,7 @@ def process_file(filepath):
     print("check point 2")  ##############
 
     #model_path = '/app/SubQAvail_model/Final_Saved_Model_LinearSVC.joblib'
-    joblib.dump(Classifier(), "Final_Saved_Model.joblib")
+    #joblib.dump(Classifier(), "Final_Saved_Model.joblib")
     model_path = '/app/SubQAvail_model/Final_Saved_Model.joblib'
     try:
         model = joblib.load(model_path)
